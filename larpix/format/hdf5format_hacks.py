d5546785e358c3be0d419e5f198a5661acaca949
Matt Kramer <matt314159@gmail.com>
2022-12-16 23:38:15 -0800

    numba-ify

--- larpix/format/hdf5format_hacks.py
+++ larpix/format/hdf5format_hacks.py
@@ -1,6 +1,7 @@
 import time
 
 import h5py
+import numba
 import numpy as np
 
 from .hdf5format import dtypes, init_file
@@ -10,14 +11,17 @@ DTYPE = dtypes[VERSION]["packets"]
 BUFSIZE = 100000
 
 
-def parse_msg(msg: np.array, io_group=0) -> np.array:
+@numba.njit
+def parse_msg(msg: np.array, packets: np.array, io_group=0) -> np.array:
+    "packets is output parameter"
     assert msg[0] == ord("D")
     pacman_timestamp = msg[1] | (msg[2] << 8) | (msg[3] << 16) | (msg[4] << 24)
+    # skip msg[5]
     nwords = msg[6] | (msg[7] << 8)
 
     npackets = nwords + 1
     # packets = np.zeros((npackets,), dtype=DTYPE)
-    packets = []
+    # packets = []
 
     for i in range(npackets):
         io_channel = 0
@@ -119,57 +123,58 @@ def parse_msg(msg: np.array, io_group=0) -> np.array:
         #     receipt_timestamp,
         # )
 
-        # packets[i][0] = io_group
-        # packets[i][1] = io_channel
-        # packets[i][2] = chip_id
-        # packets[i][3] = packet_type
-        # packets[i][4] = downstream_marker
-        # packets[i][5] = parity
-        # packets[i][6] = valid_parity
-        # packets[i][7] = channel_id
-        # packets[i][8] = timestamp
-        # packets[i][9] = dataword
-        # packets[i][10] = trigger_type
-        # packets[i][11] = local_fifo
-        # packets[i][12] = shared_fifo
-        # packets[i][13] = register_address
-        # packets[i][14] = register_data
-        # packets[i][15] = direction
-        # packets[i][16] = local_fifo_events
-        # packets[i][17] = shared_fifo_events
-        # packets[i][18] = counter
-        # packets[i][19] = fifo_diagnostics_enabled
-        # packets[i][20] = first_packet
-        # packets[i][21] = receipt_timestamp
+        packets[i][0] = io_group
+        packets[i][1] = io_channel
+        packets[i][2] = chip_id
+        packets[i][3] = packet_type
+        packets[i][4] = downstream_marker
+        packets[i][5] = parity
+        packets[i][6] = valid_parity
+        packets[i][7] = channel_id
+        packets[i][8] = timestamp
+        packets[i][9] = dataword
+        packets[i][10] = trigger_type
+        packets[i][11] = local_fifo
+        packets[i][12] = shared_fifo
+        packets[i][13] = register_address
+        packets[i][14] = register_data
+        packets[i][15] = direction
+        packets[i][16] = local_fifo_events
+        packets[i][17] = shared_fifo_events
+        packets[i][18] = counter
+        packets[i][19] = fifo_diagnostics_enabled
+        packets[i][20] = first_packet
+        packets[i][21] = receipt_timestamp
 
-        packets.append(
-            (
-                np.uint8(io_group),
-                np.uint8(io_channel),
-                np.uint8(chip_id),
-                np.uint8(packet_type),
-                np.uint8(downstream_marker),
-                np.uint8(parity),
-                np.uint8(valid_parity),
-                np.uint8(channel_id),
-                np.uint64(timestamp),
-                np.uint8(dataword),
-                np.uint8(trigger_type),
-                np.uint8(local_fifo),
-                np.uint8(shared_fifo),
-                np.uint8(register_address),
-                np.uint8(register_data),
-                np.uint8(direction),
-                np.uint8(local_fifo_events),
-                np.uint16(shared_fifo_events),
-                np.uint32(counter),
-                np.uint8(fifo_diagnostics_enabled),
-                np.uint8(first_packet),
-                np.uint32(receipt_timestamp),
-            )
-        )
+        # packets.append(
+        #     (
+        #         np.uint8(io_group),
+        #         np.uint8(io_channel),
+        #         np.uint8(chip_id),
+        #         np.uint8(packet_type),
+        #         np.uint8(downstream_marker),
+        #         np.uint8(parity),
+        #         np.uint8(valid_parity),
+        #         np.uint8(channel_id),
+        #         np.uint64(timestamp),
+        #         np.uint8(dataword),
+        #         np.uint8(trigger_type),
+        #         np.uint8(local_fifo),
+        #         np.uint8(shared_fifo),
+        #         np.uint8(register_address),
+        #         np.uint8(register_data),
+        #         np.uint8(direction),
+        #         np.uint8(local_fifo_events),
+        #         np.uint16(shared_fifo_events),
+        #         np.uint32(counter),
+        #         np.uint8(fifo_diagnostics_enabled),
+        #         np.uint8(first_packet),
+        #         np.uint32(receipt_timestamp),
+        #     )
+        # )
 
-    return packets
+    # return packets
+    return npackets
 
 
 def to_file_quick(filename, msg_list=[], io_groups=[], chip_list=[], mode="a"):
@@ -189,23 +194,33 @@ def to_file_quick(filename, msg_list=[], io_groups=[], chip_list=[], mode="a"):
             packet_dset = f[packet_dset_name]
             start_index = packet_dset.shape[0]
 
-        packets = []
+        packets = np.zeros(shape=(2*BUFSIZE,), dtype=DTYPE)
+        npackets = 0
 
         def write():
-            nonlocal packets
+            # nonlocal packets
+            nonlocal npackets
             nonlocal start_index
-            packet_dset.resize(packet_dset.shape[0] + len(packets), axis=0)
-            packet_dset[start_index:] = packets
+            packet_dset.resize(packet_dset.shape[0] + npackets, axis=0)
+            packet_dset[start_index:] = packets[:npackets]
             start_index += len(packets)
-            packets = []
+            npackets = 0
 
         for msg, io_group in zip(msg_list, io_groups):
             # packets = parse_msg(msg, io_group)
             # packet_dset.resize(start_index + len(packets), axis=0)
             # packet_dset[start_index:] = packets
             # start_index += len(packets)
-            packets.extend(parse_msg(msg, io_group))
-            if len(packets) > BUFSIZE:
+
+            # packets.extend(parse_msg(msg, io_group))
+            # if len(packets) > BUFSIZE:
+            #     write()
+
+            num_new_pkts = parse_msg(msg, packets[npackets:], io_group)
+            # packets[npackets:npackets+len(newpackets)] = newpackets
+            npackets += num_new_pkts
+            if npackets > BUFSIZE:
                 write()
+
 
         write()
